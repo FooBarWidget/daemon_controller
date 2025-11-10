e76943a8e1a6cfedd978a8fe271706ad0f8dc793
Hongli Lai <hongli@hongli.nl>
2025-11-10 14:46:11 +0100

    address comments

--- lib/daemon_controller.rb
+++ lib/daemon_controller.rb
@@ -461,37 +461,46 @@ class DaemonController
     rescue SystemCallError
     end
 
-    block = proc do
-      allow_timeout do
-        if is_direct_child
-          begin
-            debug "Waiting directly for process #{pid}"
-            Process.waitpid(pid)
-          rescue SystemCallError
-          end
-
-          # The daemon may have:
-          # 1. Written a PID file before forking. We delete this PID file.
-          #    -OR-
-          # 2. It might have forked (and written a PID file) right before
-          #    we terminated it. We'll want the fork to stay alive rather
-          #    than going through the (complicated) trouble of killing it.
-          #    Don't touch the PID file.
-          pid2 = read_pid_file
-          debug "PID file contains #{pid2.inspect}"
-          delete_pid_file if pid == pid2
-        else
-          debug "Waiting until daemon is no longer running"
-          wait_until { !daemon_is_running? }
+    begin
+      timeoutable(@start_abort_timeout) do
+        allow_timeout do
+          wait_for_aborted_process(pid:, is_direct_child:)
         end
       end
-    end
+    rescue Timeout::Error
+      begin
+        Process.kill("SIGKILL", pid)
+      rescue SystemCallError
+      end
 
-    timeoutable(@start_abort_timeout, &block)
-  rescue Timeout::Error
-    Process.kill("SIGKILL", pid)
-    block.call
-  rescue SystemCallError
+      allow_timeout do
+        wait_for_aborted_process(pid:, is_direct_child:)
+      end
+    end
+  end
+
+  def wait_for_aborted_process(pid:, is_direct_child:)
+    if is_direct_child
+      begin
+        debug "Waiting directly for process #{pid}"
+        Process.waitpid(pid)
+      rescue SystemCallError
+      end
+
+      # The daemon may have:
+      # 1. Written a PID file before forking. We delete this PID file.
+      #    -OR-
+      # 2. It might have forked (and written a PID file) right before
+      #    we terminated it. We'll want the fork to stay alive rather
+      #    than going through the (complicated) trouble of killing it.
+      #    Don't touch the PID file.
+      pid2 = read_pid_file
+      debug "PID file contains #{pid2.inspect}"
+      delete_pid_file if pid == pid2
+    else
+      debug "Waiting until daemon is no longer running"
+      wait_until { !daemon_is_running? }
+    end
   end
 
   def save_log_file_information
